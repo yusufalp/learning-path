ebedb25fe6eab507662ddeee288c13cf2f6c4d0b
yusufalp <alpyusuf007@gmail.com>
2026-04-12 12:22:08 -0700

    add forms for login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { useAuth } from "../auth/useAuth";
+
 export default function Login() {
-  return <h1>Login Page</h1>;
+  const { login } = useAuth();
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await login(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import { useAuth } from "../auth/useAuth";
+
 export default function Signup() {
-  return <h1>Signup Page</h1>;
+  const { signup } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signup(email, password, confirmPassword);
+  };
+
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button>Signup</button>
+      </form>
+    </div>
+  );
 }
