4ea820e867899c25c4173785ee4e322ca5a31418
yusufalp <alpyusuf007@gmail.com>
2026-04-12 12:21:08 -0700

    add signup method, refactor

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 
+const AUTH_API_URL = import.meta.env.VITE_AUTH_API_URL;
+
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const url = "http://localhost:8080/auth";
+    const url = `${AUTH_API_URL}/checkAuth`;
     const options = { credentials: "include" };
 
     fetch(url, options)
       .then((res) => (res.ok ? res.json() : null))
-      .then((data) => setUser(data))
+      .then((data) => setUser(data.user))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, []);
 
+  const signup = async (email, password, confirmPassword) => {
+    const url = `${AUTH_API_URL}/signup`;
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ email, password, confirmPassword }),
+    };
+
+    const res = await fetch(url, options);
+
+    if (res.ok) {
+      const data = await res.json();
+      setUser(data.user);
+    }
+  };
+
   const login = async (email, password) => {
-    const url = "http://localhost:8080/auth/login";
+    const url = `${AUTH_API_URL}/login`;
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -29,12 +48,12 @@ export function AuthProvider({ children }) {
 
     if (res.ok) {
       const data = await res.json();
-      setUser(data);
+      setUser(data.user);
     }
   };
 
   const logout = async () => {
-    const url = "http://localhost:8080/auth/login";
+    const url = `${AUTH_API_URL}/logout`;
     const options = { method: "POST", credentials: "include" };
 
     await fetch(url, options);
@@ -43,7 +62,7 @@ export function AuthProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading }}>
+    <AuthContext.Provider value={{ user, signup, login, logout, loading }}>
       {children}
     </AuthContext.Provider>
   );
