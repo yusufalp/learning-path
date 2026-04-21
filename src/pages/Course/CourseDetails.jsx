4b2fc740a42decbd7fb302a006839ac9efbee59e
yusufalp <alpyusuf007@gmail.com>
2026-04-20 22:17:38 -0700

    add boilerplate pages

--- src/pages/Course/CourseDetails.jsx
+++ src/pages/Course/CourseDetails.jsx
@@ -1,3 +1,36 @@
-export default function CourseDetails(){
-    return <h1>Course Details Page</h1>
+import { useParams } from "react-router";
+import courses from "../../data/courses.json";
+import profiles from "../../data/profiles.json";
+import { getFullName } from "../../utils/profile";
+
+export default function CourseDetails() {
+  const { courseId } = useParams();
+  console.log(courseId);
+
+  const course = courses.find((course) => course.id === courseId);
+
+  const instructor = profiles.find(
+    (profile) => profile.user_id === course.instructor_user_id,
+  );
+
+  const instructorFullName = getFullName(
+    instructor.first_name,
+    instructor.middle_name,
+    instructor.last_name,
+  );
+
+  console.log("instructor :>> ", instructor);
+  return (
+    <div>
+      <h1>Course Details Page</h1>
+
+      <p>
+        <strong>{course.title}</strong> by {instructorFullName}
+      </p>
+      <p>
+        <strong>Description: </strong>
+        {course.description}
+      </p>
+    </div>
+  );
 }
--- src/pages/Course/Courses.jsx
+++ src/pages/Course/Courses.jsx
@@ -1,3 +1,20 @@
-export default function Courses(){
-    return <h1>Courses Page</h1>
+import { Link } from "react-router";
+import courses from "../../data/courses.json";
+
+export default function Courses() {
+  console.log("courses :>> ", courses);
+  return (
+    <div>
+      <h1>Courses Page</h1>
+
+      <ul>
+        {courses.map((course) => (
+          <li>
+            <Link to={course.id}>{course.title}</Link>
+            <p>{course.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
