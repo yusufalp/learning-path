b6b6f805f9376edc6968005d494ddc0386dae2e0
yusufalp <alpyusuf007@gmail.com>
2026-04-20 22:17:00 -0700

    update routes to not to use nested routing

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -10,10 +10,10 @@ import Signup from "../pages/Signup";
 
 import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import DashboardSettings from "../pages/Dashboard/DashboardSettings";
 import Profile from "../pages/Profile/Profile";
 import ProfileSettings from "../pages/Profile/ProfileSettings";
 
-import NotFound from "../pages/NotFound";
 import Courses from "../pages/Course/Courses";
 import CourseDetails from "../pages/Course/CourseDetails";
 import CourseDetailModules from "../pages/Course/CourseDetailModules";
@@ -23,6 +23,8 @@ import CourseDetailAssignmentDetails from "../pages/Course/CourseDetailAssignmen
 import CourseDetailGrades from "../pages/Course/CourseDetailGrades";
 import CourseDetailPeople from "../pages/Course/CourseDetailPeople";
 import CourseDetailAnnouncements from "../pages/Course/CourseDetailAnnouncements";
+
+import NotFound from "../pages/NotFound";
 
 export const router = createBrowserRouter([
   {
@@ -55,15 +57,17 @@ export const router = createBrowserRouter([
         children: [
           {
             path: "dashboard",
-            Component: Dashboard,
+            children: [
+              { index: true, Component: Dashboard },
+              { path: "settings", Component: DashboardSettings },
+            ],
           },
           {
             path: "profile",
-            Component: Profile,
-          },
-          {
-            path: "profile/settings",
-            Component: ProfileSettings,
+            children: [
+              { index: true, Component: Profile },
+              { path: "settings", Component: ProfileSettings },
+            ],
           },
           {
             path: "courses",
