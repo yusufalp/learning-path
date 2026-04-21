b7043483a8b529dda928fc878b5ab1a6d5b1c7ea
yusufalp <alpyusuf007@gmail.com>
2026-04-20 22:17:53 -0700

    refactor link

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,9 +3,10 @@ import { Outlet, Link } from "react-router";
 export default function Dashboard() {
   return (
     <div>
+      <nav>
+        <Link to="settings">Edit Dashboard</Link>
+      </nav>
       <h1>Dashboard</h1>
-      <Link to="settings">Go to Settings</Link>
-      <Outlet />
     </div>
   );
 }
