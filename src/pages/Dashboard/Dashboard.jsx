255383ba71855d15656adcaa95ebae18cb306a56
yusufalp <alpyusuf007@gmail.com>
2026-05-09 00:02:10 -0700

    change links to buttons

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,15 @@
-import { Outlet, Link } from "react-router";
+import { Outlet, Link, useNavigate } from "react-router";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <nav>
-        <Link to="settings">Edit Dashboard</Link>
-      </nav>
       <h1>Dashboard</h1>
+
+      <button onClick={() => navigate("/application")}>View Application</button>
+
+      <button onClick={() => navigate("/profile")}>View Profile</button>
     </div>
   );
 }
