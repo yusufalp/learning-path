609e36908fba457de7cb8bfa6d8ec8a009785b22
yusufalp <alpyusuf007@gmail.com>
2026-05-11 21:57:24 -0700

    implement reusable button component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,24 @@
 import { Outlet, Link, useNavigate } from "react-router";
 
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+
 export default function Dashboard() {
   const navigate = useNavigate();
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Card title="Dashboard">
+        <div className="flex flex-col justify-between gap-3 sm:flex-row sm:gap-0">
+          <Button variant="secondary" onClick={() => navigate("/application")}>
+            View Application
+          </Button>
 
-      <button onClick={() => navigate("/application")}>View Application</button>
-
-      <button onClick={() => navigate("/profile")}>View Profile</button>
+          <Button variant="secondary" onClick={() => navigate("/profile")}>
+            View Profile
+          </Button>
+        </div>
+      </Card>
     </div>
   );
 }
