1fe95c5dd961222ed80274e4200c91bbbe3293f7
yusufalp <alpyusuf007@gmail.com>
2026-05-11 21:56:30 -0700

    add constants

---
+++ src/constants/navigation.js
@@ -0,0 +1,15 @@
+export const mainLinks = [
+  { to: "/", label: "Home" },
+  { to: "/about", label: "About" },
+];
+
+export const authenticatedLinks = [
+  { to: "/dashboard", label: "Dashboard" },
+  { to: "/application", label: "Application" },
+  { to: "/profile", label: "Profile" },
+];
+
+export const guestLinks = [
+  { to: "/login", label: "Login" },
+  { to: "/signup", label: "Signup" },
+];
